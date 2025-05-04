b204f560c44bc6d26aedd576bce1a448fa57684c
Jay Rathore <jayrathore88155@gmail.com>
2025-05-04 13:17:56 +0530

    Create q66.cpp

---
+++ Assignment-5/Practical/q66.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+int main(){
+    ifstream file("data.txt");
+    if(!file){
+        cerr<<"Error";
+        return 1;
+    }
+
+    string l;
+    while(getline(file , l)){
+        cout<<l<<endl;
+    }
+    return 0;
+}
